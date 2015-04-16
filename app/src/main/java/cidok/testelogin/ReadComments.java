ae8e37911484ec4fcd7c7366859d432c3cf422f7
caiopenachini <penachinicaio@hotmail.com>
2015-04-16 18:18:22 +0200

    Initial commit

---
+++ app/src/main/java/cidok/testelogin/ReadComments.java
@@ -0,0 +1,14 @@
+package cidok.testelogin;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by NOVO on 15/04/2015.
+ */
+public class ReadComments extends ActionBarActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.read_comments);
+    }
+}
